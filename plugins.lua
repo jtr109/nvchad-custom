7efabde6496853b2947908303354af6977737cbb
Ryan Li <conbas2019@gmail.com>
2023-04-24 13:33:24 +0800

    feat: init

---
+++ plugins.lua
@@ -0,0 +1,23 @@
+local plugins = {
+  -- In order to modify the `lspconfig` configuration:
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "html-lsp",
+        "prettier",
+        "stylua",
+        "pyright",
+      },
+    },
+  }
+}
+return plugins
